f138d31bec28bdef6bdeca68c19e69e0d0effee5
Lily <Lilyclutch5@gmail.com>
2025-04-18 11:16:07 +0300

    Delete cute bunny.py

--- cute bunny.py
+++
@@ -1,5 +0,0 @@
-## Welcome to My Pandas Project
-
-![Cute Bunny](https:/media.giphy.com/media/3ori000Ed9QIDllqo/giphy.gif)
-
-This project includes beginner pandas examples like creating DataFrames,selecting columns,and more.
