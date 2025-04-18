0a850e333bf43564e0407754ccbc300f302251da
Lily <Lilyclutch5@gmail.com>
2025-04-18 11:12:21 +0300

    cutie bunny

---
+++ cute bunny.py
@@ -0,0 +1,5 @@
+## Welcome to My Pandas Project
+
+![Cute Bunny](https:/media.giphy.com/media/3ori000Ed9QIDllqo/giphy.gif)
+
+This project includes beginner pandas examples like creating DataFrames,selecting columns,and more.
